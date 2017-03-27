9e0f40a39fe5af453c07557547ebe042afe34dc3
rswong <ziud7rs@gmail.com>
2017-03-27 12:28:38 +0800

    bebug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "HAVE a GOOD DAY!"
+    flash[:alert] = "LOVE YOUR LIFE!"
+    flash[:warning] = "TAKE CARE YOURSELF!"
+  end
 end
